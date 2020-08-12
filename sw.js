aedfff1f8248d1d4ffc19273a791ed610f8b72e5
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-08-12 09:35:12 +0200

    modificaciones para IOS

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/sw-utils.js');
 
 
 // variables cache
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dinamyc-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
